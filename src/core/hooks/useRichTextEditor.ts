c6d44027f70af224491b30d792304b4b1ecbda4e
Doug MacKenzie <doug.mackenzie@cultureamp.com>
2022-03-04 13:58:18 +1100

    feat: Add existing React wrapper for ProseMirror

---
+++ src/core/hooks/useRichTextEditor.ts
@@ -0,0 +1,95 @@
+import { CommandOrTransaction } from "../types.js"
+import { EditorState } from "prosemirror-state"
+import { createRichTextEditor } from "../create.js"
+import { useCallback, useEffect, useRef, useState } from "react"
+
+type Options = {
+  editable: boolean
+}
+
+type SetEditableStatus = (status: boolean) => void
+
+type UseRichTextEditorReturnValue = [
+  any,
+  EditorState,
+  (commandOrTransaction: CommandOrTransaction) => void,
+  SetEditableStatus
+]
+
+/**
+ * useRichTextEditor
+ * React hook to initialize a ProseMirror editor, handle binding it to the DOM,
+ * and updating the state within React’s lifecycle.
+ *
+ * @param {initialEditorState} ProseMirror state
+ * @returns {Array}
+ */
+export function useRichTextEditor(
+  initialEditorState: EditorState,
+  options?: Options
+): UseRichTextEditorReturnValue {
+  options = {
+    editable: true,
+    ...options,
+  }
+  const [editorState, setEditorState] =
+    useState<EditorState>(initialEditorState)
+  // Refs to hold the methods returned from ProseMirror’s initialization
+  const destroyEditorRef = useRef<() => void>()
+  const dispatchTransactionRef = useRef<
+    (commandOrTransaction: CommandOrTransaction) => void
+  >(() => undefined)
+
+  // Construct a consistent reference to call the dispatchTransactionRef without
+  // forcing the consumer to unwind it
+  const dispatchTransaction = useCallback(
+    (commandOrTransaction: CommandOrTransaction) => {
+      dispatchTransactionRef.current(commandOrTransaction)
+    },
+    [dispatchTransactionRef]
+  )
+
+  // Hold editableStatus as a ref so we can toggle its status
+  const editableStatusRef = useRef<boolean>(options.editable)
+  const setEditableStatus = useCallback<SetEditableStatus>(
+    status => {
+      editableStatusRef.current = status
+      // Trigger an update within ProseMirror by issuing a noop transaction
+      dispatchTransaction((state, dispatch) => {
+        dispatch(state.tr)
+        return true
+      })
+    },
+    [editableStatusRef]
+  )
+
+  const editorRef = useCallback(
+    (node: HTMLElement) => {
+      if (node !== null) {
+        const instance = createRichTextEditor({
+          node,
+          initialEditorState: editorState,
+          onChange: setEditorState,
+          isEditable: () => editableStatusRef.current,
+        })
+        destroyEditorRef.current = instance.destroy
+        dispatchTransactionRef.current = instance.dispatchTransaction
+      }
+    },
+
+    // Including editorState in the dependencies here will cause an endless
+    // loop as the initialization changes its value
+    [setEditorState, editableStatusRef]
+  )
+
+  // Tear down ProseMirror when the consuming component is unmounted
+  useEffect(() => {
+    return () => {
+      if (destroyEditorRef.current) {
+        destroyEditorRef.current()
+      }
+    }
+  }, [destroyEditorRef])
+
+  return [editorRef, editorState, dispatchTransaction, setEditableStatus]
+}
