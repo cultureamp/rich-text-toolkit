a3bec79a2288761e82494d0e9ec12c8aaf850f7e
Doug MacKenzie <doug.mackenzie@cultureamp.com>
2022-05-05 09:29:02 +1000

    feat: Add list nodes

---
+++ .changeset/famous-coats-jam.md
@@ -0,0 +1,5 @@
+---
+"@cultureamp/rich-text-toolkit": minor
+---
+
+Added list nodes
--- src/schema/nodes.ts
+++ src/schema/nodes.ts
@@ -1,6 +1,10 @@
 import { NodeSpec } from "prosemirror-model"
+import { bulletList, listItem, orderedList } from "prosemirror-schema-list"
 import { nodes as proseNodes } from "prosemirror-schema-basic"
 
 export const nodes: NodeSpec = {
   ...proseNodes,
+  ordered_list: orderedList,
+  bullet_list: bulletList,
+  list_item: listItem,
 }
