9a27d2da8b95233577c09180e6fca76f9dce97f0
Doug MacKenzie <doug.mackenzie@cultureamp.com>
2022-06-14 11:34:41 +1000

    Add types.d file to silence icon typing issues

---
+++ types.d.ts
@@ -0,0 +1,12 @@
+declare module "*.scss" {
+  const classes: { [key: string]: string }
+  export default classes
+}
+
+declare module "*.icon.svg" {
+  const content: {
+    id: string
+    viewBox: string
+  }
+  export default content
+}
