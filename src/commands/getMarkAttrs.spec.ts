b7006819ef031ea5094af11e70a62b8d0ad7164a
mcwinter07 <mcwinter07@gmail.com>
2022-09-01 15:55:57 +1000

    update phrasing in test description for getMarkAttrs

--- src/commands/getMarkAttrs.spec.ts
+++ src/commands/getMarkAttrs.spec.ts
@@ -10,7 +10,7 @@ describe("getMarkAttrs", () => {
   const onChange = jest.fn()
   const attributes = { "aria-labelledby": "label-text-123" }
 
-  it("returns an empty object if there selected Mark has no attributes", async () => {
+  it("returns an empty object if the selected Mark has no attributes", async () => {
     const node = document.createElement("div")
     const { dispatchTransaction } = createRichTextEditor({
       node,
