b1062e604d2262e20cfc559d1b9835bf6cf25b6e
mcwinter07 <mcwinter07@gmail.com>
2022-09-01 15:50:01 +1000

    add new test for curser in between mark

--- src/commands/markIsActive.spec.ts
+++ src/commands/markIsActive.spec.ts
@@ -2,7 +2,10 @@ import { EditorState } from "prosemirror-state"
 import { createRichTextEditor } from "../core/create"
 import { describe, expect, it, jest } from "@jest/globals"
 import { markIsActive } from "./markIsActive"
-import { simulateSelectionByText } from "./fixtures/helpers"
+import {
+  simulateSelectionByText,
+  simulateRangeSelection,
+} from "./fixtures/helpers"
 import { testEditorStateWithMarks, testSchema } from "./fixtures/test-state"
 import { waitFor } from "@testing-library/dom"
 
@@ -21,6 +24,37 @@ describe("markIsActive", () => {
     let currentState = testEditorStateWithMarks
 
     dispatchTransaction(simulateSelectionByText("Example Italic Mark"))
+    dispatchTransaction((editorState: EditorState) => {
+      currentState = editorState
+      return true
+    })
+
+    await waitFor(() => {
+      expect(markIsActive(currentState, testSchema.marks.em)).toBe(true)
+    })
+  })
+
+  it("returns true if cursor is resting within a mark (ie: Example Ital|ic Mark)", async () => {
+    const node = document.createElement("div")
+    const { dispatchTransaction } = createRichTextEditor({
+      node,
+      onChange,
+      attributes,
+      initialEditorState: testEditorStateWithMarks,
+    })
+    let currentState = testEditorStateWithMarks
+
+    dispatchTransaction(simulateSelectionByText("Example Ital"))
+    dispatchTransaction((editorState: EditorState) => {
+      currentState = editorState
+      return true
+    })
+    dispatchTransaction(
+      simulateRangeSelection(
+        currentState.selection.to,
+        currentState.selection.to
+      )
+    )
     dispatchTransaction((editorState: EditorState) => {
       currentState = editorState
       return true
